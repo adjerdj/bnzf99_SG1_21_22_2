f3caf604348c21339e0a5950cb51b6f725ce92eb
adjerdj <andrej.djerdj@gmail.com>
2022-05-25 05:54:09 +0200

    Added code to .Endpoint

--- BNZF99_SG1_21_22_2/BNZF99_SG1_21_22_2.Endpoint/Controllers/ArtistController.cs
+++ BNZF99_SG1_21_22_2/BNZF99_SG1_21_22_2.Endpoint/Controllers/ArtistController.cs
@@ -1,15 +1,123 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BNZF99_SG1_21_22_2.Logic.Interfaces;
+using BNZF99_SG1_21_22_2.Models.DTOs;
+using BNZF99_SG1_21_22_2.Models.Entities;
+using BNZF99_SG1_21_22_2.Models.Enums;
+using BNZF99_SG1_21_22_2.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BNZF99_SG1_21_22_2.Endpoint.Controllers
 {
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ArtistController : ControllerBase
     {
+        readonly IArtistLogic artistLogic;
+
+        public ArtistController(IArtistLogic artistLogic)
+        {
+            this.artistLogic = artistLogic;
+        }
+
+        // GET: api/Artist/GetAll
+        [HttpGet]
+        [ActionName("GetAll")]
+        public IEnumerable<Artist> Get()
+        {
+            return artistLogic.ReadAll();
+        }
+
+        // GET api/Artist/Get/5
+        [HttpGet("{id}")]
+        public Artist Get(int id)
+        {
+            return artistLogic.Read(id);
+        }
+
+        // POST api/Artist
+        [HttpPost]
+        [ActionName("Create")]
+        public ApiResult Post(ArtistDTO artist)
+        {
+            var result = new ApiResult(true);
+
+            try
+            {
+                artistLogic.Create(new Artist()
+                {
+                    Id = artist.Id,
+                    Name = artist.Name,
+                    Born = artist.Born,
+                    RecordLabel = artist.RecordLabel,
+                    Instrument = artist.Instrument,
+                    IsOnRehab = artist.IsOnRehab,
+                    EndOfContract = artist.EndOfContract
+                });
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccessful = false;
+                result.Messages = new List<string> { ex.Message };
+            }
+
+            return result;
+        }
+
+        // PUT api/Artist/Update
+        [HttpPut]
+        [ActionName("Update")]
+        public ApiResult Put(ArtistDTO artist)
+        {
+            var result = new ApiResult(true);
+
+            try
+            {
+                artistLogic.Update(new Artist()
+                {
+                    Id = artist.Id,
+                    Name = artist.Name,
+                    Born = artist.Born,
+                    RecordLabel = artist.RecordLabel,
+                    Instrument = artist.Instrument,
+                    IsOnRehab = artist.IsOnRehab,
+                    EndOfContract = artist.EndOfContract
+
+                });
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccessful = false;
+                result.Messages = new List<string> { ex.Message };
+            }
+
+            return result;
+        }
+
+        // DELETE api/Artist/5
+        [HttpDelete("{id}")]
+        public ApiResult Delete(int id)
+        {
+            var result = new ApiResult(true);
+
+            try
+            {
+                artistLogic.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccessful = false;
+                result.Messages = new List<string> { ex.Message };
+            }
+
+            return result;
+        }
+
+        [HttpGet]
+        public IEnumerable<Instruments> GetAllInstruments()
+        {
+            return (Instruments[])Enum.GetValues(typeof(Instruments));
+        }
     }
 }
--- BNZF99_SG1_21_22_2/BNZF99_SG1_21_22_2.Endpoint/Startup.cs
+++ BNZF99_SG1_21_22_2/BNZF99_SG1_21_22_2.Endpoint/Startup.cs
@@ -1,36 +1,21 @@
+using BNZF99_SG1_21_22_2.Logic.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BNZF99_SG1_21_22_2.Endpoint
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BNZF99_SG1_21_22_2.Endpoint", Version = "v1" });
-            });
+            services.AddCors();
+
+            BLInitialization.InitBlServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,13 +24,15 @@ namespace BNZF99_SG1_21_22_2.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BNZF99_SG1_21_22_2.Endpoint v1"));
             }
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseCors(x =>
+            x.AllowAnyMethod()
+            .AllowAnyHeader()
+            .SetIsOriginAllowed(origin => true)
+            .AllowCredentials());
 
             app.UseEndpoints(endpoints =>
             {
